f51304a40b2ae54882bc013cb675768da15094a8
jyotirepo <jsethy2010@gmail.com>
2019-08-12 11:16:10 +0530

    commit

--- prechecks.sh
+++ prechecks.sh
@@ -8,29 +8,3 @@ if command -v python &>/dev/null; then
 else
     echo Python  is not installed
 fi
-SPACE=`df -h`
-KERNALV=`uname -r`
-KERNELHEADER=`rpm -qa | grep -i kernel-header`
-LISTENINGPORTS=`iptables -L -n`
-BOOTLOADER=`grub-install --version`
-echo -ne "\n"
-echo "1. STORAGE DETAILS :
-*********************************************
-      $SPACE "
-echo -ne "\n"
-echo "2. KERNEL VERSION :
-*********************************************
-      $KERNALV "
-echo -ne "\n"
-echo "3. KERNEL HEADER VERSIONS :
-*********************************************
-      $KERNELHEADER "
-echo -ne "\n"
-echo "4. LISTENING PORTS :
-*********************************************
-      $LISTENINGPORTS "
-echo -ne "\n"
-echo "5. BOOT LOADER :
-*********************************************
-      $BOOTLOADER "
-echo -ne "\n"
